3ecf72e523321b7fc0eba04524e666792985ef1c
Dan <daweidern@gmail.com>
2022-06-29 11:04:11 -0500

    commit tree 262835a97dfda11f7e41544971d9edc0077ab889

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// code your solution here
+function saturdayFun(activity){
+    if (activity) {
+        return `This Saturday, I want to ${activity}!`;
+    }
+    else {
+        return 'This Saturday, I want to roller-skate!';
+    }
+}
+
+function mondayWork(activity){
+    if (activity){
+        return `This Monday, I will ${activity}.`
+    }   
+    else {
+        return 'This Monday, I will go to the office.';
+    }
+}
+
+function wrapAdjective(visualFlair = "*"){
+        return function(D = 'special'){
+            return `You are ${visualFlair}${D}${visualFlair}!`
+        }
+
+}
+
+
+
+
